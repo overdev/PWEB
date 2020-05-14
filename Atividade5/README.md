98e587a4a0bc1d34ad22ce7580e53bc13ac18f15
overdev <jorgegomes83@hotmail.com>
2020-05-13 21:38:06 -0300

    Atividade 5 (Maior de 3 números).

---
+++ Atividade5/README.md
@@ -0,0 +1,3 @@
+# Atividade 5
+
+Criar uma função que receba 3 numeros como parâmetros e retorne o  maior deles. Se dois ou três forem iguais mostra o valor igual.
---
+++ Atividade5/main.js
@@ -0,0 +1,24 @@
+
+/**
+ * Retorna o maior entre três numeros.
+ * @param a um numero
+ * @param b um numero
+ * @param c um numero
+ */
+function max3(a, b, c) {
+    let compA = a > b ? a : b;
+    let compB = compA > c ? compA : c;
+    return compB;
+}
+
+function maiorDeTres() {
+    let a = parseInt(prompt("Digite o 1º numero (inteiro):"));
+    let b = parseInt(prompt("Digite o 2º numero (inteiro):"));
+    let c = parseInt(prompt("Digite o 3º numero (inteiro):"));
+    let resultado = max3(a, b, c);
+
+    alert("O maior entre " + a + ", " + b + " e " + c +" é: " + max3(a, b, c));
+}
+
+
+maiorDeTres()
